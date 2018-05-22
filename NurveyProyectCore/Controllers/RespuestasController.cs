74f27c30b256794b8ae6550353fea9c8fe55b285
Gaston Alonso <gastondolly@gmail.com>
2018-05-22 19:47:52 -0300

    NewMappedControllers
    New controllers: Encuestados / Respuestas

---
+++ NurveyProyectCore/Controllers/RespuestasController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntidadesNurvey;
+using Microsoft.AspNetCore.Mvc;
+using NurveyProyectCore.Services;
+using Newtonsoft.Json;
+
+namespace NurveyProyectCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class RespuestasController : Controller
+    {
+        private RespuestasRepository respuestasRepository;
+
+        public RespuestasController()
+        {
+            this.respuestasRepository = new RespuestasRepository();
+        }
+
+        [HttpGet]
+        public Respuestas[] Get()
+        {
+            return respuestasRepository.getAllRespuestas();
+        }
+
+        [HttpPost]
+        public IActionResult Post(RespuestaEncuestado respuestaEncuestado)
+        {
+            bool guardado = this.respuestasRepository.SaveRespuesta(respuestaEncuestado);
+
+            string respuesta = "";
+
+            //var responseJSON = "";
+            //HttpResponseMessage response;
+
+            if (guardado)
+            {
+                //responseJSON = "{\"status\":\"OK\",\"message\":\"Encuesta contestada exitosamente\"";
+                //response = Request.CreateResponse(System.Net.HttpStatusCode.Created, responseJSON);
+
+                respuesta = "Encuesta contestada exitosamente";
+            }
+            else
+            {
+                //responseJSON = "{\"status\":\"ERROR\",\"message\":\"No se guardo la encuesta\"";
+                //response = Request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, responseJSON);
+
+                respuesta = "No se guardó la encuesta";
+            }
+
+            return Ok(respuesta);
+        }
+    }
+}
---
+++ NurveyProyectCore/Services/EncuestadosRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAONurvey;
+using EntidadesNurvey;
+
+namespace NurveyProyectCore.Services
+{
+    public class EncuestadosRepository
+    {
+        public Encuestado[] getAllEncuestados()
+        {
+            return DAOEncuestado.ObtenerEncuestados();
+        }
+
+        public bool SaveEncuestados(Encuestado encuestado)
+        {
+            try
+            {
+                DAOEncuestado.InsertarEncuestado(encuestado);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+    }
+}
