abc847236be2774f7009f895fd120669db858626
Gaston Alonso <gastondolly@gmail.com>
2018-05-21 21:35:49 -0300

    FirstCommitFromLenovo
    the connection string was changed for local lenovo computer and csproj was added to gitignore

--- DAONurvey/CadenaConexion.cs
+++ DAONurvey/CadenaConexion.cs
@@ -6,7 +6,8 @@ namespace DAONurvey
 {
     public class CadenaConexion
     {
-        public static string cadenaConexion = "Data Source=nurveyback.sytes.net;Integrated Security=False;User ID=NurveyDB;Password=ADM357357*+;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //public static string cadenaConexion = "Data Source=nurveyback.sytes.net;Integrated Security=False;User ID=NurveyDB;Password=ADM357357*+;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         //public static string cadenaConexion = "Data Source=DOLLY-NOTEBOOK\\SQLEXPRESS;Initial Catalog=NurveyDB_Prod2;Integrated Security=True";
+        public static string cadenaConexion = "Data Source=DOLLY-LENOVO\\sqlexpress;Initial Catalog=NurveyDB_Prod2;Integrated Security=True";
     }
 }
