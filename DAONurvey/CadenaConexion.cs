03f4419f960122401ff5190aef8351ddd64d9511
Gaston Alonso <gastondolly@gmail.com>
2018-07-11 19:05:57 -0300

    conexionBDServerLocal

--- DAONurvey/CadenaConexion.cs
+++ DAONurvey/CadenaConexion.cs
@@ -6,11 +6,11 @@ namespace DAONurvey
 {
     public class CadenaConexion
     {
-        //public static string cadenaConexion = "Data Source=nurveyback.sytes.net;Integrated Security=False;User ID=NurveyDB;Password=ADM357357*+;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+         public static string cadenaConexion = "Data Source=nurveyback.sytes.net;Integrated Security=False;User ID=NurveyDB;Password=ADM357357*+;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         //public static string cadenaConexion = "Data Source=DOLLY-NOTEBOOK\\SQLEXPRESS;Initial Catalog=NurveyDB_Prod2;Integrated Security=True";
         //public static string cadenaConexion = "Data Source=DOLLY-LENOVO\\sqlexpress;Initial Catalog=NurveyDB_Prod2;Integrated Security=True";
         //public static string cadenaConexion = "Data Source=nurveydbs.c3m9fifuzzkq.sa-east-1.rds.amazonaws.com,1433;Integrated Security=False;User ID=NurveyD;Password=ADM357357*+;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        public static string cadenaConexion = "Data Source=nurveydbs.c3m9fifuzzkq.sa-east-1.rds.amazonaws.com,1433;Initial Catalog=NurveyD;User ID=NurveyDB;Password=ADM357357*+";
+        //public static string cadenaConexion = "Data Source=nurveydbs.c3m9fifuzzkq.sa-east-1.rds.amazonaws.com,1433;Initial Catalog=NurveyD;User ID=NurveyDB;Password=ADM357357*+";
 
 
     }
